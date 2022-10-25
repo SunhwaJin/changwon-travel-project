76fea80493a30b3689dfdaccbf7ab38eade0639d
SunhwaJin <116528663+SunhwaJin@users.noreply.github.com>
2022-10-25 14:30:50 +0900

    Create Spots.java

---
+++ Changwon/src/main/java/com/example/Changwon/DTO/Spots.java
@@ -0,0 +1,72 @@
+package com.example.Changwon.DTO;
+
+public class Spots {
+	
+	private int aid;
+	private String name;
+	private String address;
+	private String desc;
+	private String number;
+	private String time;
+	private String img;
+	
+	
+	public int getAid() {
+		return aid;
+	}
+
+	public void setAid(int aid) {
+		this.aid = aid;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String location) {
+		this.address = location;
+	}
+
+
+	public String getDesc() {
+		return desc;
+	}
+
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+
+	public String getNumber() {
+		return number;
+	}
+
+	public void setNumber(String number) {
+		this.number = number;
+	}
+
+	public String getTime() {
+		return time;
+	}
+
+	public void setTime(String time) {
+		this.time = time;
+	}
+
+	public String getImg() {
+		return img;
+	}
+
+	public void setImg(String img) {
+		this.img = img;
+	}
+	
+	
+}
